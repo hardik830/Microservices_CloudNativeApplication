791c6799835446be2f20ea85ecbc80b82c8b9348
hardik830 <hardkhandelwal9636@gmail.com>
2024-11-15 16:02:56 +0530

    Emplement feature related to eureka server and open feign client

---
+++ microservices/section8/eurekaserver/src/main/java/com/eazybytes/eurekaserver/EurekaserverApplication.java
@@ -0,0 +1,17 @@
+package com.eazybytes.eurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+//by this annotation telling spring boot configure the application as normal spring boot project
+//to enable eureka server from the spring cloud project
+@EnableEurekaServer
+public class EurekaserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaserverApplication.class, args);
+	}
+
+}
